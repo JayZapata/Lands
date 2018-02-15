aa68ce2097eb500e921a5579edeccc5d831e7ca4
JayZapata <jayzapatamax@gmail.com>
2018-02-14 23:31:25 -0500

    Conection whit binding

--- Lands/Lands/ViewModels/MainViewModel.cs
+++ Lands/Lands/ViewModels/MainViewModel.cs
@@ -6,5 +6,23 @@ namespace Lands.ViewModels
 {
     public class MainViewModel
     {
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            this.Login = new LoginViewModel();
+        }
+
+        #endregion
+
     }
 }

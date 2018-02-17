1bde42f1f438d1ee89c27c1b0fa06210a4ee36d5
JayZapata <jayzapatamax@gmail.com>
2018-02-17 11:17:03 -0500

    ApiServices

--- Lands/Lands/ViewModels/LandsViewModel.cs
+++ Lands/Lands/ViewModels/LandsViewModel.cs
@@ -1,6 +1,70 @@
 ﻿namespace Lands.ViewModels
 {
-    public class LandsViewModel
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using Services;
+    using Xamarin.Forms;
+
+    public class LandsViewModel:BaseViewModel
     {
+        #region Services
+        private ApiService apiServices;
+        #endregion
+
+
+        #region Attributes
+        private ObservableCollection<Land> lands;
+
+        #endregion
+
+
+        #region Properties
+
+        public ObservableCollection<Land> Lands
+        {
+            get { return this.lands; }
+            set { this.SetValue(ref this.lands, value); }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public LandsViewModel()
+        {
+            this.apiServices = new ApiService();
+            this.LoadLands();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private async void LoadLands()
+        {
+            var response=await this.apiServices.GetList<Land>(
+                "http://restcountries.eu/rest/v2/all",
+                "/rest",
+                "/v2/all");
+
+            if (!response.IsSuccess)
+                {
+                //this.IsRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    response.Message,
+                    "Accept");
+                return;
+               }
+            var list = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(list);
+        }
+
+
+        #endregion
+
+
     }
 }

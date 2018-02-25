5d4801daa1e2ef5c52c59b50bf3ad009b1758ab2
JayZapata <jayzapatamax@gmail.com>
2018-02-25 13:57:12 -0500

    Finish land tabbed page

--- Lands/Lands/ViewModels/LandViewModel.cs
+++ Lands/Lands/ViewModels/LandViewModel.cs
@@ -12,17 +12,16 @@ namespace Lands.ViewModels
     {
         #region Attributes
         private ObservableCollection<Borders> borders;
-        
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
         #endregion
-
 
         #region Properties
         public Land Land
         {
             get;
             set;
-        }
-        #endregion
+        }        
 
         public ObservableCollection<Borders> Borders
         {
@@ -30,12 +29,27 @@ namespace Lands.ViewModels
             set { this.SetValue(ref this.borders, value); }
         }
 
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { this.SetValue(ref this.currencies, value); }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get { return this.languages; }
+            set { this.SetValue(ref this.languages, value); }
+        }
+        #endregion
+
         #region Constructors
 
         public LandViewModel(Land land)
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
 
         #endregion
